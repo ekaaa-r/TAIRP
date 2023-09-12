664c09f599552f6407d2efb1136b7c5f60da3a86
Ekta Rai <114333756+ekaaa-r@users.noreply.github.com>
2023-09-12 20:25:18 +0530

    Add files via upload

---
+++ to do list/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const input = document.querySelector('input');
+    const btn = document.querySelector('.addTask > button');
+
+    btn.addEventListener('click', addList);
+    input.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            addList(e);
+        }
+    });
+
+    function addList() {
+        const notCompleted = document.querySelector('.notCompleted');
+        const Completed = document.querySelector('.Completed');
+
+        const newLi = document.createElement('li');
+        const checkBtn = document.createElement('button');
+        const delBtn = document.createElement('button');
+
+        checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+        delBtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+        
+        checkBtn.classList.add('check-button');
+
+        if (input.value !== '') {
+            newLi.textContent = input.value;
+            input.value = '';
+            notCompleted.appendChild(newLi);
+            newLi.appendChild(checkBtn);
+            newLi.appendChild(delBtn);
+        }
+
+        checkBtn.addEventListener('click', function() {
+            const parent = this.parentNode;
+            parent.remove();
+            Completed.appendChild(parent);
+
+            
+            const checkButton = parent.querySelector('.check-button');
+            checkButton.style.display = 'none';
+        });
+
+        delBtn.addEventListener('click', function() {
+            const parent = this.parentNode;
+            parent.remove();
+        });
+    }
+});
